e1a90f83bd2e8430fe3f715707fa2ffbc4b7b006
Auro Tripathy <ipserv@yahoo.com>
2023-06-30 06:04:56 +0900

    1st working version

--- compile_run.py
+++ compile_run.py
@@ -5,6 +5,7 @@ from furiosa import runtime
 from furiosa.runtime import session
 import numpy as np
 import cv2
+from utils import utils_image as util
 from pudb import set_trace
 
 LOGLEVEL = os.environ.get('FURIOSA_LOG_LEVEL', 'INFO').upper()
@@ -23,7 +24,7 @@ def run_example():
     # Generate the random input tensor according to the input shape
     img_path = 'testsets/set12/01.png'
     img = cv2.imread(img_path)
-    set_trace()
+    util.imsave(img, 'noised.png')    
     img = img.transpose(2, 0, 1).astype("float32")
     input = img[np.newaxis, :, :, :]
     
@@ -31,9 +32,12 @@ def run_example():
     outputs = sess.run(input)
     
     print("== Output ==")
-    print(outputs)
+    print(f'Tensor output shape:\n{outputs}')
+    set_trace()
     print(outputs[0].numpy())
-
+    print(outputs[0].shape)
+    cv2.imwrite('denoised.png',
+                np.squeeze(outputs[0].numpy()).transpose(1,2,0)) 
 
 if __name__ == "__main__":
     run_example()
