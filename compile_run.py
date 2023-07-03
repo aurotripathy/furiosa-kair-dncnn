ea3013933cc0cde9e7da5caa3ff024638b8c91d0
Auro Tripathy <ipserv@yahoo.com>
2023-07-04 04:24:16 +0900

    forcing resize

--- compile_run.py
+++ compile_run.py
@@ -17,11 +17,13 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('--quant_model_path', required=True, type=str)
 parser.add_argument('--noise_level_img', type=int, default=15, help='noise level: 15, 25, 50')
+parser.add_argument('--show_img', type=bool, default=False, help='show the image')
 args = parser.parse_args()
 
 LOGLEVEL = os.environ.get('FURIOSA_LOG_LEVEL', 'INFO').upper()
 logging.basicConfig(level=LOGLEVEL)
 
+img_size = (256, 256)
 def run_example():
     runtime.__full_version__
 
@@ -32,18 +34,20 @@ def run_example():
     input_tensor = sess.inputs()[0]
 
     img_paths = util.get_image_paths('testsets/set12/')
-    set_trace()
 
     for img_path in img_paths:
         
         in_img = cv2.imread(img_path)
+        in_img = cv2.resize(in_img, img_size)
+        set_trace()
 
         # degrade
         img = util.uint2single(in_img)  # scale down [0,1]
         np.random.seed(seed=0)  # for reproducibility
         img += np.random.normal(0, args.noise_level_img/255., img.shape)
-        util.imshow(util.single2uint(img),
-                    title='Noisy image {}'.format(args.noise_level_img))
+        if args.show_img:
+            util.imshow(util.single2uint(img),
+                        title='Noisy image {}'.format(args.noise_level_img))
 
         img = util.single2uint(img)
         # set_trace()
@@ -59,8 +63,8 @@ def run_example():
 
         out_img = np.squeeze(outputs[0].numpy()).transpose(1,2,0)
         cv2.imwrite('denoised.png', out_img)
-        # util.imshow(out_img.round().astype(np.uint8), title='denoised')
-        util.imshow(out_img.round().astype(np.uint8), title='denoised')
+        if args.show_img:
+            util.imshow(out_img.round().astype(np.uint8), title='denoised')
 
         psnr = util.calculate_psnr(out_img, in_img, border=0)
         ssim = util.calculate_ssim(out_img, in_img, border=0)
